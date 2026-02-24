a7be4b228be30cf6eb220304fd079d88cdfb9a95
alishasedai <alishasedai21@gmail.com>
2026-02-24 18:43:48 +0545

    arrays in js learned

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log("Hello, World!");
+
+// forEach
+var arr = [1, 2, 3, 4, 5];
+
+arr.forEach((a) => {
+    console.log(a+2);
+})
+
+//map
+var arr2 = arr.map( (a) => {
+    return a *3
+})
+console.log(arr2);
+
+//filter
+var arr3 = arr.filter((a) => {
+    if(a >=4){
+        return true;
+    }
+    else{
+        return false;
+    }
+})
+console.log(arr3);
+
+//find
+
+var arr4 = arr.find((a) => {
+    if(a ===90){
+        return true;
+    }
+    else{
+        return false;
+    }
+})
+console.log(arr4);
+//IndexOf
+var arr5 = arr.indexOf(30);
+console.log(arr5);
