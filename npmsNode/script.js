324d0077cb4c3ee2504f57a45cd10c249696c5f6
alishasedai <alishasedai21@gmail.com>
2026-02-28 13:56:46 +0545

    npm fileSystem learning

---
+++ npmsNode/script.js
@@ -0,0 +1,20 @@
+// const fs = require("fs");
+// fs.writeFile("hey.txt", "hello Alsiha Tum kaise ho", function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Done");
+//   }
+// });
+const fs = require("fs");
+fs.appendFile(
+  "hey.txt",
+  " Hi i am fine but i dont remeber you who are you",
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Done");
+    }
+  },
+);
