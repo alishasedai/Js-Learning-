22300ff4ca3c0d5379ebf4561bd3b90741233659
alishasedai <alishasedai21@gmail.com>
2026-02-28 17:25:16 +0545

    making the http server

--- npmsNode/script.js
+++ npmsNode/script.js
@@ -48,7 +48,14 @@ const fs = require("fs");
 
 //reading the file from the folder
 
-fs.readFile("./copy/index.html", "utf8", function (err, data) {
-  if (err) console.log(err);
-  else console.log(data);
-});
+// fs.readFile("./copy/index.html", "utf8", function (err, data) {
+//   if (err) console.log(err);
+//   else console.log(data);
+// });
+
+
+const http = require("http");
+const server = http.createServer(function(req,res){
+    res.end("hello Alisha");
+})
+server.listen(3000);
