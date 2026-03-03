02faa71a6ad1e1889a2116931309a9671d71b907
alishasedai <alishasedai21@gmail.com>
2026-03-03 09:51:16 +0545

    create,delete,update by making the routes

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
+const userModel = require("./usermodel");
 
 app.get("/", function(req,res){
-    res.send("heheheh")
+    res.send("heheheh");
+});
+app.get("/create", async function (req, res) {
+   let createdUser = await userModel.create({
+        name :"Anu Dahal",
+        username : "anuD",
+        email : "anu@gmail.com"
+    })
+    res.send(createdUser)
+});
+app.get("/update",async function(req,res){
+    let updateUser = await userModel.findOneAndUpdate({username:"alishas"},{name:"Alishma Thapa"},{new:true});
+    res.send(updateUser)
 })
+app.get("/read", async function (req, res) {
+  let readUser = await userModel.find();
+  res.send(readUser);
+});
+
 app.listen(3000);
