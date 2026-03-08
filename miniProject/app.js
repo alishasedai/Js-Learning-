36e043b0b044d596983e3c44fb9822a17ded545a
alishasedai <alishasedai21@gmail.com>
2026-03-08 21:45:22 +0545

    mini project login logout using the middleware

--- miniProject/app.js
+++ miniProject/app.js
@@ -36,5 +36,43 @@ app.post("/register",async function(req,res){
     })
     
 })
+
+app.get("/login",function(req,res){
+    res.render("login")
+})
+app.get("/profile",isLoggedIn,function(req,res){
+    console.log(req.user);
+    res.render("login")
+})
+app.post("/login",async function(req,res){
+    let {email,password} =req.body;
+    let user =await userModel.findOne({email});
+    if(!user) return res.status(500).send("Something went wrong");
+
+    bcrypt.compare(password,user.password,function(err,result){
+        if(result)  {
+            let token = jwt.sign({email:email,userid:user._id},"shh");
+            res.cookie("token",token)
+            res.status(200).send("You can login");}
+        else{
+            res.send("Your password is incorrect");
+        }
+        
+    })
+    
+})
+app.get("/logout",function(req,res){
+    res.cookie("token","");
+    res.redirect("/login")
+});
+function isLoggedIn(req,res,next){
+    if(req.cookies.token === "") return res.send("You must be logged in");
+    else{
+      let data = jwt.verify(req.cookies.token,"shh") ;
+      req.user = data
+       next();
+    }
+   
+}
 app.listen(3000)
 
