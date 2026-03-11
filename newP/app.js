636fd2276f8451923eb69a7f638b6687e2de81e4
alishasedai <alishasedai21@gmail.com>
2026-03-11 17:24:43 +0545

    projects setup

--- newP/app.js
+++ newP/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const path = requitre("path")
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs");
 
 app.get("/",function(req,res){
     res.send("Hello")
---
+++ newP/models/user-model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/scatch");
+
+const userSchema = mongoose.Schema({
+    fullname :String,
+    email:String,
+    password:String,
+    cart:{
+        type:Array,
+        default:[]
+    },
+    isAdmin :Boolean,
+    orders:{
+        types:Array,
+        default:[]
+    },
+    contact:Number,
+    picture:String
+})
+module.exports = mongoose.model("user",userSchema);
