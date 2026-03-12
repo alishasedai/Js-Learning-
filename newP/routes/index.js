f55f43e6b6e2f380c4a96024c2e5de9ad5bed5f5
alishasedai <alishasedai21@gmail.com>
2026-03-12 17:22:29 +0545

    creating the user an storing at the mongodb

---
+++ newP/routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+router.get("/",function(req,res){
+    res.send("Hey its working from the index router")
+})
+
+module.exports = router;
--- newP/routes/usersRouter.js
+++ newP/routes/usersRouter.js
@@ -1,6 +1,22 @@
 const express= require("express");
 const router = express.Router();
+const userModel = require("../models/user-model")
 router.get("/",function(req,res){
   res.send("hey its working form userRouter")  
 });
+
+router.post("/register",async function(req,res){
+  let { email,password } =req.body
+ try{
+  let createdUser = await userModel.create({
+    fullname,email,password
+  })
+   res.send(createdUser);
+ }
+
+ catch(err){
+  res.send(err.message)
+ }
+  
+})
 module.exports = router
