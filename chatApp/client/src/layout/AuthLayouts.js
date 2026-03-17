9a060235bba200e992583661b7b01eebf3383dec
alishasedai <alishasedai21@gmail.com>
2026-03-17 16:28:56 +0545

    designing the frontedd

--- chatApp/client/src/layout/AuthLayouts.js
+++ chatApp/client/src/layout/AuthLayouts.js
@@ -3,8 +3,8 @@ import logo from '../assets/images.png'
 const AuthLayouts = ({children}) => {
   return (
     <>
-      <header className='flex justify-center items-center py-3 px-5 h-25 shadow-md'>
-        <img src={logo} alt="logo" width={130} height={20}></img>
+      <header className='flex justify-center items-center py-3 px-5 h-20 shadow-md bg-white'>
+        <img src={logo} alt="logo" width={100} height={10}></img>
       </header>
       {children}
     </>
--- chatApp/client/src/pages/RegisterPage.js
+++ chatApp/client/src/pages/RegisterPage.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const RegisterPage = () => {
+  const {data,setData} = useState({
+    name : "",
+    email : "",
+    password : "",
+    profile_pic : ""
+  })
+  const handleOnChange = (e) => {
+    const {name,value} = e.target
+  }
   return (
-    <div>
-      <div>
-        <h3>
-          Welcome to Chat App!
-        </h3>
-        <form>
-          
+    <div className="mt-5">
+      <div className="bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4">
+        <h3>Welcome to Chat App!</h3>
+        <form className="grid gap-4">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="enter your name"
+              className="bg-slate-100 px-2 py-1 focus:outline-blue-300 h-10"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="enter your email"
+              className="bg-slate-100 px-2 py-1 focus:outline-blue-300 h-10"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="enter your password"
+              className="bg-slate-100 px-2 py-1 focus:outline-blue-300 h-10"
+            />
+          </div>
         </form>
       </div>
     </div>
