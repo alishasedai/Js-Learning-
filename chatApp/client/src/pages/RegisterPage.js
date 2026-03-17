d923511be09b88b44185361c4f3e05f3edf6c096
alishasedai <alishasedai21@gmail.com>
2026-03-17 23:06:05 +0545

    creating the form

--- chatApp/client/src/pages/RegisterPage.js
+++ chatApp/client/src/pages/RegisterPage.js
@@ -2,20 +2,26 @@ import React, {useState} from 'react'
 
 
 const RegisterPage = () => {
-  const {data,setData} = useState({
+  const [data,setData] = useState({
     name : "",
     email : "",
     password : "",
     profile_pic : ""
   })
   const handleOnChange = (e) => {
-    const {name,value} = e.target
+    const {name,value} = e.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name] : value
+      }
+    })
   }
   return (
     <div className="mt-5">
       <div className="bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4">
         <h3>Welcome to Chat App!</h3>
-        <form className="grid gap-4">
+        <form className="grid gap-4 mt-5">
           <div className="flex flex-col gap-1">
             <label htmlFor="name">Name:</label>
             <input
@@ -23,6 +29,9 @@ const RegisterPage = () => {
               name="name"
               id="name"
               placeholder="enter your name"
+              value={data.name}
+              onChange={handleOnChange}
+              required
               className="bg-slate-100 px-2 py-1 focus:outline-blue-300 h-10"
             />
           </div>
@@ -33,6 +42,9 @@ const RegisterPage = () => {
               name="email"
               id="email"
               placeholder="enter your email"
+              value={data.email}
+              onChange={handleOnChange}
+              required
               className="bg-slate-100 px-2 py-1 focus:outline-blue-300 h-10"
             />
           </div>
@@ -43,6 +55,9 @@ const RegisterPage = () => {
               name="password"
               id="password"
               placeholder="enter your password"
+              value={data.password}
+              onChange={handleOnChange}
+              required
               className="bg-slate-100 px-2 py-1 focus:outline-blue-300 h-10"
             />
           </div>
