4296e767b42ad9ed9cbdf6f6ff495b05ef2af0df
alishasedai <alishasedai21@gmail.com>
2026-03-25 12:03:57 +0545

    searching the user

---
+++ chatApp/client/src/components/SearchUser.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SearchUser = () => {
+  return <div>openSearchUser</div>;
+}
+
+export default SearchUser
--- chatApp/client/src/components/Sidebar.js
+++ chatApp/client/src/components/Sidebar.js
@@ -6,13 +6,17 @@ import { NavLink } from 'react-router-dom';
 import Avatar from "./Avatar"
 import { useDispatch, useSelector } from "react-redux";
 import EditUserDetails from './EditUserDetails';
-
+import Divider from './Divider';
+import { FiArrowUpLeft } from "react-icons/fi";
+import SearchUser from './SearchUser';
 
 const Sidebar = () => {
   const user = useSelector(state => state?.user);
   const [editUserOpen, setEditUserOpen] = useState(false)
+  const [allUser,setAllUser] = useState([])
+  const [openSearchUser,setOpenSearchUser] = useState(false)
   return (
-    <div className="w-full h-full grid grid-cols-[48px,1fr]">
+    <div className="w-full h-full grid grid-cols-[48px,1fr] bg-white">
       <div className="bg-slate-200 flex flex-col justify-between rounded-tr-lg rounded-br-lg w-12 h-full py-5">
         <div>
           <div
@@ -23,8 +27,11 @@ const Sidebar = () => {
           </div>
           <NavLink
             title="add friend"
+            onClick={() => {
+              setOpenSearchUser(true)
+            }}
             className={({ isActive }) =>
-              `w-12 h-12 hover:bg-slate-300 rounded flex justify-center items-center cursor-pointer ${isActive && "bg-yellow-600"}`
+              `w-12 h-12 hover:bg-slate-300 rounded flex justify-center items-center cursor-pointer ${isActive && "bg-slate-400"}`
             }
           >
             <FaUserPlus size={20} />
@@ -36,7 +43,12 @@ const Sidebar = () => {
             title={user.name}
             onClick={() => setEditUserOpen(true)}
           >
-            <Avatar width={50} height={50} name={user.name} imageUrl={user?.profile_pic}/>
+            <Avatar
+              width={50}
+              height={50}
+              name={user.name}
+              imageUrl={user?.profile_pic}
+            />
           </button>
           <button
             title="logout"
@@ -48,14 +60,33 @@ const Sidebar = () => {
           </button>
         </div>
       </div>
-            <div className='w-full bg-red-500'>
-              Sidebar details
+      <div className="w-full">
+        <div className="h-18 flex items-center">
+          <h2 className="text-2xl font-bold p-4 ">Message</h2>
+        </div>
+        <div className="bg-slate-200 p-[0.5px]"></div>
+        <div className=" h-[calc(100vh-70px)] overflow-x-hidden overflow-y-auto scrollbar">
+          {allUser.length === 0 && (
+            <div className="mt-12">
+              <div className="flex justify-center items-center my-4 text-slate-500">
+                <FiArrowUpLeft size={50} />
+              </div>
+              <p className="text-lg text-center text-slate-600">
+                Explore users to start a convo
+              </p>
             </div>
+          )}
+        </div>
+      </div>
       {/*Edit user details*/}
+      {editUserOpen && (
+        <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />
+      )}
+      {/* search user */}
       {
-        editUserOpen && (
-          <EditUserDetails onClose={() => setEditUserOpen(false)} user={user}/>
-        )
+      openSearchUser && (
+        <SearchUser onClose={() => setOpenSearchUser(false)}/>
+      )
       }
     </div>
   );
