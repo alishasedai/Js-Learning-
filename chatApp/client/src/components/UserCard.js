1eeae2c421c72354effe5eaa2263593b59be184d
alishasedai <alishasedai21@gmail.com>
2026-03-28 22:37:54 +0545

    displaying the user cards

--- chatApp/client/src/components/UserCard.js
+++ chatApp/client/src/components/UserCard.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import Avatar from './Avatar';
 
-const UserCard = () => {
-  return <div>UserCard</div>;
+const UserCard = ({user}) => {
+  return <div>
+    <div>
+      <Avatar
+      width={50}
+      height={50}
+      name={user?.name}/>
+    </div>
+    <div>
+      <div className='font-semibold'>
+        {user?.name}
+      </div>
+      <p>{user?.email}</p>
+    </div>
+  </div>;
 }
 
 export default UserCard
