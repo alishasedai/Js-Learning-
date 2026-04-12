1814e339ca54c2f3483a312630dc334f21cc4be5
alishasedai <alishasedai21@gmail.com>
2026-04-12 10:03:30 +0545

    multiple events using

--- reactLearn/vite-project/src/Bye.jsx
+++ reactLearn/vite-project/src/Bye.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import styles from "./Bye.module.css"
+import anju from "./Bye.module.css"
 
-const Bye = () => {
+const Bye = () => {  
+    // function getName(yourname){
+    //     return yourname
+    // }  
+function handleClick (){
+    alert("You clicked me ...");
+
+}
+function handleInput (event){
+    // console.clear();
+    console.log("value",event.target.value)
+}
+const handleMouseOver = () =>{
+    console.log("Mouse is over the text.");
+
+}
+const  handleDoubleClick = () =>{
+    console.log("Double click is done..")
+}
+    //using the arrow function
+   const getName = (yourname) => {
+        return yourname
+    }
+    const name = "Arju Sedai...";
+    const name2 = "Alisha Sedai...";
   return (
     <div>
-      <h2 className={styles.testing}>Sanvi Sedai</h2>
+      <h2 className={anju.testing}>Sanvi Sedai</h2>
+      <h1>Hello {getName(name)}</h1>
+      <h3>Hello {getName(name2)}</h3>
+      <p onMouseOver={handleMouseOver} onDoubleClick={handleDoubleClick}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit error veritatis laboriosam deserunt. ut quos qui inventore.</p>
+      <button onClick={handleClick}>Click Me..</button><br />
+      <button onClick={() => {alert("hello from inline function")}}>Hello</button><br />
+      <input type="text" onChange={handleInput} placeholder='enter some text..' />
+
     </div>
   )
 }
