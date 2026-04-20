ac9e282d30e3dd7db374c3d6efed5f69596e976c
alishasedai <alishasedai21@gmail.com>
2026-04-20 19:07:45 +0545

    router learn

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -3,14 +3,25 @@ import First from './First'
 import Timer from './Timer'
 import WindowSizeTracker from './WindowSizeTracker'
 import Users from './Users' 
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 const App = () => {
   return (
-    <div>
-      {/* <First/>
-      <Timer/> */}
-      {/* <WindowSizeTracker/> */}
-      <Users />
-    </div>
+    // <div>
+    //   {/* <First/>
+    //   <Timer/> */}
+    //   {/* <WindowSizeTracker/> */}
+    //   <Users />
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
